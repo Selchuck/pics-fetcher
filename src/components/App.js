374a84a5d621f3a58d0caa911c00bc300367ed3a
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-01-28 15:58:17 +0100

    add state object to App component to handle images data

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+    state = { images: [] };
+
     async onSearchSubmit(term) {
         const response = await axios
             .get('https://api.unsplash.com/search/photos', {
@@ -13,13 +15,14 @@ class App extends React.Component {
                     Authorization: 'Client-ID L_GPQXotYLuLBEQEn5EM_TbV0VTm9C4MEbMtOkoVBTU'
                 }
             });
-            console.log(response.data)
+            this.setState({ images: response.data.results })
         }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
                 <SearchBar onQuerrySubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
             </div>
         );
     }
