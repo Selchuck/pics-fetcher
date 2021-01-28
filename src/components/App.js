eadab36d0d6b0599459e50ce8e50261199a675f7
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-01-28 16:38:15 +0100

    create api directory and unsplash.js configuration file to handle axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     state = { images: [] };
 
     onSearchSubmit = async (term) => {
-        const response = await axios
-            .get('https://api.unsplash.com/search/photos', {
-                params: {
-                    query: term
-                },
-                headers: {
-                    Authorization: 'Client-ID L_GPQXotYLuLBEQEn5EM_TbV0VTm9C4MEbMtOkoVBTU'
-                }
+        const response = await unsplash.get('/search/photos', {
+                params: { query: term }
             });
-            
+
         this.setState({ images: response.data.results })
     }
 
